bb6b662f36dcead604bdf759986ed4b80ccfbf06
Shawn Seguin <shawn4seg@gmail.com>
2023-09-23 11:10:45 -0400

    Merge Modif couleurs

--- lechai-frontend-client/src/app/app-routing.module.ts
+++ lechai-frontend-client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './components/accueil/accueil.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 import { PanierComponent } from './panier/panier.component';
 import { ListeSouhaitComponent } from './liste-souhait/liste-souhait.component';
 import { CommandesDetailsComponent } from './commandes-details/commandes-details.component';
