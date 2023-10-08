29d7c6c9017862c24956fbee85e8567833c32403
ame0406 <123507685+ame0406@users.noreply.github.com>
2023-10-08 10:09:27 -0400

    animation carousel

--- lechai-frontend-client/src/app/components/accueil/carousel-accueil/carousel-accueil.component.ts
+++ lechai-frontend-client/src/app/components/accueil/carousel-accueil/carousel-accueil.component.ts
@@ -39,7 +39,7 @@ export class CarouselAccueilComponent implements OnInit {
   startCarousel(): void {
     this.intervalId = setInterval(() => {
       this.changeImg(1); // Change l'image automatiquement
-    }, 2000);
+    }, 6000);
   }
 
   // Méthode pour arrêter le carrousel automatique
